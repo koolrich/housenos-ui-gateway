1a7714486699857fd37efa4caa6f169942553309
Richard Nduka <koolrich@gmail.com>
2019-06-02 00:49:58 +0100

    Initial setup - react and spring boot

---
+++ housenos-gateway/src/main/java/com/housenos/housenosgateway/HousenosGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.housenos.housenosgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousenosGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousenosGatewayApplication.class, args);
+	}
+
+}
