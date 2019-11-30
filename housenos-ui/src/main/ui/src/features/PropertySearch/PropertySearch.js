69d98279a4ea492fb619e7aa91e69d43a54a330d
Valentin Ciobanu <valentinciobanu@Valentins-MacBook-Pro.local>
2019-11-30 20:56:40 +0200

    commit1 on searchPage

--- housenos-ui/src/main/ui/src/features/PropertySearch/PropertySearch.js
+++ housenos-ui/src/main/ui/src/features/PropertySearch/PropertySearch.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './index.css';
+import { Badge } from 'reactstrap';
+import { Form, Input } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 function PropertySearch(props) {
     return(
         <div className="container">
-            <p>Property search goes here</p>
+            <div className="searchContainer">
+                <div className="text-center">
+                    <h1 className="mainHeading">Reimagine home <Badge color="danger">New</Badge></h1>
+                    <h3 className="secondaryHeading">Let's help you find your dream home</h3>
+                </div>
+                <Row className="pt-5">
+                    <Col m="12" md={{ size: 6, offset: 3 }}>
+                        <Form>
+                            <Input placeholder="Enter an address, town or city" bsSize="default" />
+                        </Form>
+                    </Col>      
+                </Row>  
+            </div>        
         </div>
+        
     )
 }
 
